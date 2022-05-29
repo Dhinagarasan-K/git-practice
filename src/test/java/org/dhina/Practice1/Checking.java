1bd991c42e92c5073018df77e2b79da36753bf7c
Dhinagarasan K <dhinagarasank@gmail.com>
2022-05-29 08:49:00 +0530

    first commit

---
+++ src/test/java/org/dhina/Practice1/Checking.java
@@ -0,0 +1,17 @@
+package org.dhina.Practice1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Checking {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("first code");
+		
+		
+		
+		
+	}
+
+}
